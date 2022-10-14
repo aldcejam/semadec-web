9c52662685535e4061eadbdbebd1e826f912c75c
Aldcejam <aldcejamj923@gmail.com>
2022-10-14 10:56:29 -0300

    s

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       {/*  */}
       <DefaultBackground />
       <ContainerGlobal>
-        <SettingsButtons />
       </ContainerGlobal>
       {/*  */}
       <ContainerPage>
