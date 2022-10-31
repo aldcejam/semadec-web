ce73b9d99c6c28dd40c537393ccb1e0bd683c893
Aldcejam <aldcejamj923@gmail.com>
2022-10-31 11:06:14 -0300

    lazy home card

--- src/components/templates/HomePageCards/Index.tsx
+++ src/components/templates/HomePageCards/Index.tsx
@@ -1,30 +1,43 @@
-
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react';
 import IllustrationCourse from "../../../../public/illustrationsForCard/IllustrationCourse/IllustrationCourse"
 import IllustrationEdicoesDaSemadec from "../../../../public/illustrationsForCard/IllustrationEdicoesDaSemadec/IllustrationEdicoesDaSemadec"
 import IllustrationJogosDoSeuCurso from "../../../../public/illustrationsForCard/IllustrationJogosDoSeuCurso/IllustrationJogosDoSeuCurso"
 import IllustrationJogosEsportivos from "../../../../public/illustrationsForCard/IllustrationJogosEsportivos/IllustrationJogosEsportivos"
 import IllustrationPublicacoesSobreSemadec from "../../../../public/illustrationsForCard/IllustrationPublicacoesSobreSemadec/IllustrationPublicacoesSobreSemadec"
 import IllustrationRecordesDestaEdicao from "../../../../public/illustrationsForCard/IllustrationRecordesDeTodosOsAnos/IllustrationRecordesDeTodosOsAnos"
-import HomePageCard from "../../Organisms/HomePageCards/Index"
 import { StyledHomePageCards } from "./Styled"
 
+const HomePageCard = lazy(() => import("../../Organisms/HomePageCards/Index"))
 
 const HomePageCards = () => {
     return (
         <StyledHomePageCards>
             <div className="grid-top">
-                <HomePageCard link='/' title='equipes por curso' illustration={<IllustrationCourse />} />
-                <HomePageCard link='/' title='jogos esportivos' illustration={<IllustrationJogosEsportivos />} />
-                <HomePageCard link='/findGameBySport?curso=informatica' title='jogos do seu curso' illustration={<IllustrationJogosDoSeuCurso />} />
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <HomePageCard link='/' title='equipes por curso' illustration={<IllustrationCourse />} />
+                </Suspense>
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <HomePageCard link='/' title='jogos esportivos' illustration={<IllustrationJogosEsportivos />} />
+                </Suspense>
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <HomePageCard link='/findGameBySport?curso=informatica' title='jogos do seu curso' illustration={<IllustrationJogosDoSeuCurso />} />
+                </Suspense>
             </div>
             <div className="grid-left">
-                <HomePageCard link='/' title='recordes desta edição' illustration={<IllustrationRecordesDestaEdicao />} />
-                <HomePageCard link='/' title='publicações sobre a semadec' illustration={<IllustrationPublicacoesSobreSemadec />} />
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <HomePageCard link='/' title='recordes desta edição' illustration={<IllustrationRecordesDestaEdicao />} />
+                </Suspense>
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <HomePageCard link='/' title='publicações sobre a semadec' illustration={<IllustrationPublicacoesSobreSemadec />} />
+                </Suspense>
             </div>
             <div className="grid-right">
-                <HomePageCard link='/' title='recordes de todos os anos' illustration={<IllustrationRecordesDestaEdicao />} />
-                <HomePageCard link='/' title='Edições da semadec' illustration={<IllustrationEdicoesDaSemadec />} />
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <HomePageCard link='/' title='recordes de todos os anos' illustration={<IllustrationRecordesDestaEdicao />} />
+                </Suspense>
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <HomePageCard link='/' title='Edições da semadec' illustration={<IllustrationEdicoesDaSemadec />} />
+                </Suspense>
             </div>
         </StyledHomePageCards>
     )
