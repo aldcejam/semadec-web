209de479f918e9f5ac53bac5b2ed888261cb6edb
Aldcejam <aldcejamj923@gmail.com>
2022-11-03 13:40:49 -0300

    pontuation game

---
+++ src/components/Atoms/ModalToConfigGame/SelectPontuations/Styled.tsx
@@ -0,0 +1,20 @@
+import { styled } from "@mui/material/styles"
+import { FormControl } from "@mui/material"
+
+export const StyledSelectPontuations = styled(FormControl)`
+        width: 80%;
+      .MuiOutlinedInput-root {
+          label {
+        }
+        & fieldset {
+            border: solid 2px ${(props) => props.theme.palette.primary.dark};
+            border-radius: ${(props) => props.theme.shape.borderRadius};
+        }
+        &.Mui-focused fieldset {
+            border: solid 2px ${(props) => props.theme.palette.primary.main};
+        }
+    }
+    MuiInputLabel-root {
+        padding-bottom: 10px;
+      }
+`
